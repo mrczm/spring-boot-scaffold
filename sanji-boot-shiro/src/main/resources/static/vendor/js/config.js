21d60bb703e4f6cb6f228223b8732a5a1eaad115
sunxyz <2445213759@qq.com>
2017-03-18 17:00:38 +0800

    git出错 将dist改为 vendor

---
+++ sanji-boot-shiro/src/main/resources/static/vendor/js/config.js
@@ -0,0 +1,11 @@
+var domain = '/api/'
+var config = {
+    api:{
+        role: domain+'sys/role/',
+        menu: domain+'sys/menu/',
+    },
+    frame:{
+        role:'role/frame',
+        menu:'menu/frame',
+    },
+}
---
+++ sanji-boot-shiro/src/main/resources/static/vendor/js/pages/menu_frame_page.js
@@ -0,0 +1,74 @@
+/**
+ * Created by sunxyz on 2017/3/17.
+ */
+(function () {
+    var id = 0;
+    var path = config.api.menu;
+    layui.use(['form', 'layedit', 'laydate'], function () {
+        var form = layui.form()
+            , layer = layui.layer
+
+        //自定义验证规则
+        form.verify({
+            name: function (value) {
+                if (value.length < 3) {
+                    return '名称至少得3个字符啊';
+                }
+            }
+        });
+
+        //监听提交
+        form.on('submit(demo1)', function (data) {
+            if (id) {
+                edit()
+            } else {
+                add();
+            }
+            return false;
+        });
+        var f = function () {
+            var obj = JSON.parse($("#json").val());
+            id = obj.id;
+//            console.log(obj)
+            var app = new Vue({
+                el: '#app',
+                data: {
+                    obj: obj,
+                    icon: obj.icon,
+                    isChild: obj.isParent
+                }
+
+            })
+            console.log(obj)
+            if(obj.visible){
+                $("#visible").attr("checked",true);
+                // console.log("------------------")
+            }
+            form.render();
+        }
+        setTimeout(f, 200)
+    });
+
+    function edit() {
+        var url = path + id;
+        var method = "PUT";
+        save(url, method);
+    }
+
+    function add() {
+        var url = path;
+        var method = "POST";
+        save(url, method);
+    }
+
+    function save(url, method) {
+        var options = {
+            url: url,
+            method: method,
+            success: function (data) {
+                layer.msg(data.msg)
+            }
+        };
+        $("#app").ajaxSubmit(options)
+    }
+})();
