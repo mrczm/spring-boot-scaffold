791ae7409e073497eefc8a48079288754c23000f
LiangHongwen <1317201840@qq.com>
2017-07-18 10:06:37 +0800

    菜单页面编写

--- sanji-boot-security/src/main/java/com/sj/modules/sys/repository/MenuRepository.java
+++ sanji-boot-security/src/main/java/com/sj/modules/sys/repository/MenuRepository.java
@@ -3,9 +3,15 @@ package com.sj.modules.sys.repository;
 import com.sj.modules.sys.domain.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Created by lhw on 2017/7/14.
  */
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+
+    List<Menu> findByName(String name);
+
+    List<Menu> findByParent(Menu menu);
 
 }
--- sanji-boot-security/src/main/java/com/sj/modules/sys/web/MenuController.java
+++ sanji-boot-security/src/main/java/com/sj/modules/sys/web/MenuController.java
@@ -5,11 +5,10 @@ import com.sj.modules.sys.domain.Menu;
 import com.sj.modules.sys.repository.MenuRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by lhw on 2017/7/15.
@@ -22,13 +21,27 @@ public class MenuController {
 
     @RequestMapping
     public ResponseEntity list() {
-        List<Menu> menus = menuRepository.findAll();
+        List<Menu> menus = menuRepository.findByName("ROOT");
+        menus.forEach(menu -> {
+            List<Menu> menus1 = menuRepository.findByParent(menu).stream().filter(menu1 -> !menu1.getName().equals("ROOT")).collect(Collectors.toList());
+            menu.setSubMenus(menus1);
+            menus1.forEach(menu1 -> {
+                List<Menu> menus2 = menuRepository.findByParent(menu1);
+                menu1.setSubMenus(menus2);
+            });
+        });
         System.out.println("menus " + JSON.toJSONString(menus));
         return ResponseEntity.ok(menus);
     }
 
-    @RequestMapping("{id}")
+    @GetMapping("{id}")
     public ResponseEntity findById(@PathVariable Long id) {
         return ResponseEntity.ok(menuRepository.findOne(id));
+    }
+
+    @PostMapping("/add/{id}")
+    public ResponseEntity add(@PathVariable Long id, Menu menu) {
+        menu = menuRepository.saveAndFlush(menu);
+        return ResponseEntity.ok(menu);
     }
 }
