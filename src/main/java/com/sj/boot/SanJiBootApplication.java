8a292b85de3630b7e5d2b7e835a253870e1e1140
yangrd <2445213759@qq.com>
2019-01-18 15:58:26 +0800

    update SanJiBootApplication

--- src/main/java/com/sj/boot/SanJiBootApplication.java
+++ src/main/java/com/sj/boot/SanJiBootApplication.java
@@ -1,10 +1,9 @@
 package com.sj.boot;
 
-import com.sj.boot.modules.sys.model.Menu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author yangrd
